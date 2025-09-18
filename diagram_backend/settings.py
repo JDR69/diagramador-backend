80214b3d900340b22c4da33560ae85e7d53024c9
Joan Dr <joandanielrr@gmail.com>
2025-09-17 21:26:13 -0400

    bd

--- diagram_backend/settings.py
+++ diagram_backend/settings.py
@@ -76,8 +76,12 @@ ASGI_APPLICATION = 'diagram_backend.asgi.application'
 # Base de datos
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('POSTGRES_DB', default='diagramdb'),
+        'USER': config('POSTGRES_USER', default='diagramuser'),
+        'PASSWORD': config('POSTGRES_PASSWORD', default='diagramsecret'),
+        'HOST': config('POSTGRES_HOST', default='localhost'),
+        'PORT': config('POSTGRES_PORT', default='5432'),
     }
 }
 
