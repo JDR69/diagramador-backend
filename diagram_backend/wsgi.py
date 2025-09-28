6c184b32096bf2bc97adb214275a23a548b1eec5
Joan Dr <joandanielrr@gmail.com>
2025-09-28 10:11:42 -0400

    Cambios Config

--- diagram_backend/wsgi.py
+++ diagram_backend/wsgi.py
@@ -1,4 +1,15 @@
 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+WSGI config for diagram_backend project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
 import os
 from django.core.wsgi import get_wsgi_application
 
